e829085db78b0b5205354f3e2738d2f323a22a55
rafamanetti <84095571+rafamanetti@users.noreply.github.com>
2024-05-28 18:48:49 -0300

    Add files via upload
    Translating the subtitles

--- main_def.py
+++ main_def.py
@@ -1,49 +1,49 @@
 import pandas as pd
 import logging
 
-# Configuração do logging
+# Logging Setup
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def load_and_prepare_data(file_path):
-    logging.info(f'Carregando dados do arquivo: {file_path}')
+    logging.info(f'Loading File: {file_path}')
     df = pd.read_csv(file_path)
     
-    logging.info('Convertendo coluna Date para datetime e mudando para o formato dia-mes-ano')
+    logging.info('Converting date column to datetime and changing the date format')
     df['Date'] = pd.to_datetime(df['Date'])
     df['Date'] = df['Date'].dt.strftime('%d-%m-%Y')
     
-    logging.info('Preenchendo valores nulos')
+    logging.info('Filling nulls')
     df.fillna(0, inplace=True)
     
     return df
 
 def enrich_data(df):
-    logging.info('Criando variação diária')
+    logging.info('Creating Daily Variation')
     df['Daily Variation'] = df['Close'] - df['Open']
     df['Daily Variation'] = df['Daily Variation'].round(2).astype(str) + 'k'
     
-    logging.info('Calculando variação diária em percentual')
+    logging.info('Calculating the daily variation in %')
     df['Daily Variation (%)'] = df['Close'].pct_change() * 100
     df['Daily Variation (%)'] = df['Daily Variation (%)'].round(2)
     
-    # Preencher os valores NaN com 0
+    # Filling the NaN values with 0
     df['Daily Variation (%)'].fillna(0, inplace=True)
     
-    # Converter para string e adicionar sufixo
+    # Converting to string and adding the '%'
     df['Daily Variation (%)'] = df['Daily Variation (%)'].astype(str) + ' %'
     
-    logging.info('Calculando quantas BTC foram negociadas no dia')
+    logging.info('Calculating how many BTC were traded in that day')
     df['Volume Variation'] = df['Volume'].diff().fillna(0).astype('int64')
     
-    logging.info('Criando coluna ID')
+    logging.info('Creating the ID column')
     df["id"] = df.index + 1
     
-    logging.info('Movendo a coluna ID para a primeira posição')
+    logging.info('Moving the ID column to the first column')
     cols = list(df.columns)
     cols.insert(0, cols.pop(cols.index('id')))
     df = df[cols]
     
-    logging.info('Adicionando novamente a conversão para datetime')
+    logging.info('Setting the datetime again')
     df['Date'] = pd.to_datetime(df['Date'], format='%d-%m-%Y')
     
     return df
@@ -51,22 +51,22 @@ def enrich_data(df):
 def save_by_year(df):
     for year in range(2014, 2025):
         df_year = df[df['Date'].dt.year == year]
-        logging.info(f'Ano {year}: {len(df_year)} linhas')
+        logging.info(f'Year {year}: {len(df_year)} rows')
         
         file_name = f'BTC-HISTORY-{year}.csv'
-        logging.info(f'Salvando dados do ano {year} em {file_name}')
+        logging.info(f'Saving data from year {year} in {file_name}')
         df_year.to_csv(file_name, index=False)
 
 def create_consolidated(df):
-    logging.info('Criando DataFrame consolidado')
+    logging.info('Creating a consolidated file')
     df_consolidated = df.copy()
     
     file_name = 'BTC-HISTORY-Consolidated.csv'
-    logging.info(f'Salvando DataFrame consolidado em {file_name}')
+    logging.info(f'Saving the consolidated file in {file_name}')
     df_consolidated.to_csv(file_name, index=False)
 
 def main():
-    logging.info('Iniciando o processo')
+    logging.info('Initiating the process')
     
     file_path = 'BTC-USD.csv'
     df = load_and_prepare_data(file_path)
@@ -77,7 +77,7 @@ def main():
     
     create_consolidated(df)
     
-    logging.info('Processo concluído')
+    logging.info('Success!')
     logging.info(df.head())
     logging.info(df.info())
     logging.info(df.describe())
